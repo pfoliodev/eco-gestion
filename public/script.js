a73131110389bc987c75cac1f46fce0096f61508
PHenri11 <phleguy@yahoo.fr>
2025-12-27 19:16:46 +0100

    fix style and remove dark mode

--- public/script.js
+++ public/script.js
@@ -45,8 +45,8 @@ function initTinyMCE() {
             'alignright alignjustify | bullist numlist outdent indent | ' +
             'removeformat | code | help',
         content_style: 'body { font-family:Inter,sans-serif; font-size:14px }',
-        skin: (localStorage.getItem('theme') === 'dark' ? 'oxide-dark' : 'oxide'),
-        content_css: (localStorage.getItem('theme') === 'dark' ? 'dark' : 'default')
+        skin: 'oxide',
+        content_css: 'default'
     });
 }
 
@@ -380,35 +380,8 @@ function backToCourses() {
     showPage('cours');
 }
 
-function initTheme() {
-    const themeToggle = document.getElementById('theme-toggle');
-    const body = document.body;
-    const sunIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12"x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
-    const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
-
-    let isDarkMode = localStorage.getItem('theme') === 'dark';
-
-    const applyTheme = () => {
-        if (isDarkMode) {
-            body.classList.add('dark-mode');
-            themeToggle.innerHTML = sunIcon;
-        } else {
-            body.classList.remove('dark-mode');
-            themeToggle.innerHTML = moonIcon;
-        }
-    };
-
-    themeToggle.addEventListener('click', () => {
-        isDarkMode = !isDarkMode;
-        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-        applyTheme();
-    });
-
-    applyTheme();
-}
 
 document.addEventListener('DOMContentLoaded', () => {
-    initTheme();
     initTinyMCE();
     initAuth();
     initForm();
