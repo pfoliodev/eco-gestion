aa41315c0e96b5222696841d6b1deb3d0adaf95e
PHenri11 <phleguy@yahoo.fr>
2026-01-11 18:40:09 +0100

    Style(Pantheon): Add fancy gradient tooltip for pet icon with level display

--- public/js/pantheon.js
+++ public/js/pantheon.js
@@ -200,7 +200,7 @@ function renderLeaderboard(container, users) {
             <div class="col-user">
                 <div class="user-avatar-container">
                     <img src="${user.photoURL || `https://ui-avatars.com/api/?name=${encodeURIComponent(user.displayName)}&background=random`}" class="user-avatar" alt="${user.displayName}">
-                    ${user.activePet && user.activePet.image ? `<img src="${user.activePet.image}" class="user-pet-icon" title="Compagnon: ${user.activePet.name}">` : ''}
+                    ${user.activePet && user.activePet.image ? `<img src="${user.activePet.image}" class="user-pet-icon" data-tooltip="🐾 ${user.activePet.name} (Niv. ${user.activePet.level || 1})">` : ''}
                 </div>
                 <div class="user-details">
                     <span class="user-name">${user.displayName}</span>
